219d2dac6b60e5638fbff4e884f6705301cce12e
melih <durmazmelih.ml@gmail.com>
2022-12-16 01:41:55 +0300

    deneme

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,6 @@ public class Main {
 
         System.out.println("yedinci deneme");
 
-        System.out.println("melihin yazdığı kod");
+        System.out.println("melihin yazdığı kod1");
     }
 }
